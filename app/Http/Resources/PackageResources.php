9ff00900a9f896bb1d7e7f893647a90420a3c5b6
Putheng <puthengemail@gmail.com>
2019-06-24 11:23:58 +0000

    fixed employer buy package

--- app/Http/Resources/PackageResources.php
+++ app/Http/Resources/PackageResources.php
@@ -15,6 +15,7 @@ class PackageResources extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'name' => $this->title,
             'post' => $this->post,
